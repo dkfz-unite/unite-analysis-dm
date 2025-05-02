6a550fd1ebb2097b3224b808ab1d3374ce6f82ce
vulasau <valiantsin.ulasau@gmail.com>
2025-05-02 15:11:54 +0200

    Optimisation of installation files.

--- src/install/install.R
+++ src/install/install.R
@@ -1,7 +1,26 @@
-install.packages("BiocManager")
-BiocManager::install("minfi", ask = T)
-BiocManager::install("limma", ask = T)
-BiocManager::install("IlluminaHumanMethylation450kmanifest", ask = T)
-BiocManager::install("IlluminaHumanMethylationEPICmanifest", ask = T)
-BiocManager::install("IlluminaHumanMethylationEPICanno.ilm10b4.hg19", ask = T)
-BiocManager::install("dplyr", ask = T)
+# install.packages("BiocManager")
+# BiocManager::install("minfi", ask = T)
+# BiocManager::install("limma", ask = T)
+# BiocManager::install("IlluminaHumanMethylation450kmanifest", ask = T)
+# BiocManager::install("IlluminaHumanMethylationEPICmanifest", ask = T)
+# BiocManager::install("IlluminaHumanMethylationEPICanno.ilm10b4.hg19", ask = T)
+# BiocManager::install("dplyr", ask = T)
+
+install.packages("BiocManager", dependencies = FALSE)
+# BiocManager::install("minfi", ask = FALSE, update = FALSE, dependencies = c("Depends", "Imports"))
+# BiocManager::install("limma", ask = FALSE, update = FALSE, dependencies = c("Depends", "Imports"))
+# BiocManager::install("IlluminaHumanMethylation450kmanifest", ask = FALSE, update = FALSE, dependencies = c("Depends", "Imports"))
+# BiocManager::install("IlluminaHumanMethylationEPICmanifest", ask = FALSE, update = FALSE, dependencies = c("Depends", "Imports"))
+# BiocManager::install("IlluminaHumanMethylationEPICanno.ilm10b4.hg19", ask = FALSE, update = FALSE, dependencies = c("Depends", "Imports"))
+# BiocManager::install("dplyr", ask = FALSE, update = FALSE, dependencies = c("Depends", "Imports"))
+
+packages <- c(
+    "minfi",
+    "limma",
+    "IlluminaHumanMethylation450kmanifest",
+    "IlluminaHumanMethylationEPICmanifest",
+    "IlluminaHumanMethylationEPICanno.ilm10b4.hg19",
+    "dplyr"
+)
+
+BiocManager::install(packages, ask = FALSE, update = FALSE, dependencies = c("Depends", "Imports"))
